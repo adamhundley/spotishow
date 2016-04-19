b6dfe5b80dfb6dc37d0e2afc7624b676cda00e81
Adam Hundley <adamhundley@gmail.com>
2016-04-18 20:18:30 -0600

    Home page style
    * stlyed home page
    * separated out logged in and non logged in home page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,6 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(request.env["omniauth.auth"])
     session[:uid] = user.uid
+    redirect_to root_path
   end
 end
