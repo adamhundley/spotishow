d7c5cb8cb16e77f5d16ea11346f46f028b5aa291
Adam Hundley <adamhundley@gmail.com>
2016-04-23 17:35:45 -0500

    fixed a test for show page

--- spec/features/user_views_shows_spec.rb
+++ spec/features/user_views_shows_spec.rb
@@ -18,9 +18,10 @@ RSpec.feature "UserLogsInWithSpotify", type: :feature do
 
       expect(current_path).to eq "/adamhundley/shows"
 
-      expect(page).to have_content "Lord Huron @ Red Rocks"
-      expect(page).to have_content "Ryan Adams @ Red Rocks"
-      expect(page).to have_content "The Lone Bellow @ Rocky Mountain Folks Festival"
+      expect(page).to have_content "Lord Huron"
+      expect(page).to have_content "Red Rocks"
+      expect(page).to have_content "Ryan Adams"
+      expect(page).to have_content "The Lone Bellow"
       expect(page).to_not have_content "Sting @ The Greek"
 
       expect(User.first.shows.count).to eq 11
