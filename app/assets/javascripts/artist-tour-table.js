5ee74484b93820c7544ac0aaf9d226133aadd10c
Adam Hundley <adamhundley@gmail.com>
2016-04-27 15:43:43 -0600

    New User ROw
    * fixed issue where icons didn't work on new user
    * bound to doc instead of each row
    * user can be added and removed at the same time
    
    closes #61

--- app/assets/javascripts/artist-tour-table.js
+++ app/assets/javascripts/artist-tour-table.js
@@ -1,6 +1,6 @@
 $( document).ready(function() {
 
-  $('.artist-tour').one('click', function(){
+  $(document).one('click', '.artist-tour', function(){
     var artist = $(this).attr('data-artist')
     var id = $(this).attr('data-id')
     $('.modal-spinner').show().delay(2000);
@@ -8,25 +8,23 @@ $( document).ready(function() {
     $.getJSON("/api/artists/"+artist+"", function(data){
       $.each(data, function(key, show) {
         renderShowRow(show);
-      });
-    })
-    var renderShowRow = function(show) {
-      date = new Date(show.datetime);
-      month = parseInt(date.getMonth()) + 1
-      html = "<tr class=\"show-row\">";
-      html += "<td class=\"\">" + month + "/" + date.getDate() + "</td>";
-      html += "<td class=\"\">" + show.venue_name + "</td>";
-      html += "<td class=\"\">" + show.formatted_location + "</td>";
-      html += "<td class=\"\"><a href='" + show.ticket_url + "'target=\"_blank\">Tickets</a></td>";
-      html += "<td class=\"\"><a href='" + show.facebook_rsvp_url + "'target=\"_blank\">RSVP</a></td>";
-      html += "</tr>";
-      $("#show-modal-body-"+id+"").append(html)
-    }
-      $(document).ajaxStop(function(){
-        $(".modal-spinner").hide();
-      });
+    });
+  })
 
+  var renderShowRow = function(show) {
+    date = new Date(show.datetime);
+    month = parseInt(date.getMonth()) + 1
+    html = "<tr class=\"show-row\">";
+    html += "<td class=\"\">" + month + "/" + date.getDate() + "</td>";
+    html += "<td class=\"\">" + show.venue_name + "</td>";
+    html += "<td class=\"\">" + show.formatted_location + "</td>";
+    html += "<td class=\"\"><a href='" + show.ticket_url + "'target=\"_blank\">Tickets</a></td>";
+    html += "<td class=\"\"><a href='" + show.facebook_rsvp_url + "'target=\"_blank\">RSVP</a></td>";
+    html += "</tr>";
+    $("#show-modal-body-"+id+"").append(html)
+  }
+    $(document).ajaxStop(function(){
+      $(".modal-spinner").hide();
+    });
   });
-
-
 });
